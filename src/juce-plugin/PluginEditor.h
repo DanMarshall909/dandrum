bba3a62fbb364a77e3933a7896720605cf9f8ad1
Dan Marshall <37458844+DanMarshall909@users.noreply.github.com>
2026-07-06 21:55:30 +1000

    Add generic plugin parameter controls

--- src/juce-plugin/PluginEditor.h
+++ src/juce-plugin/PluginEditor.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <memory>
+#include <vector>
+
 #include <juce_audio_processors/juce_audio_processors.h>
 
 #include "PluginProcessor.h"
@@ -14,8 +17,16 @@ public:
     void resized() override;
 
 private:
+    struct ParameterControl
+    {
+        juce::RangedAudioParameter* parameter = nullptr;
+        std::unique_ptr<juce::Label> label;
+        std::unique_ptr<juce::Slider> slider;
+    };
+
     DandrumAudioProcessor& processor;
     juce::Label statusLabel;
+    std::vector<ParameterControl> parameterControls;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (DandrumAudioProcessorEditor)
 };
